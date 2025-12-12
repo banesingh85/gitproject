9ee0cb0ebce9f7a9b755566f20269118214a86bb
banesingh85 <banesingh.85@gmail.com>
2025-12-12 22:37:47 +0530

    updatecode7

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello from Docker Node App!</h1>");
+  res.send("<h1>Hello from Docker Node App Banesingh !</h1>");
 });
 
 app.listen(PORT, () => {
